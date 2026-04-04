0af42b14d78e03d88d9bc55ca5eac03b6a810767
codephoenix86 <nareshlohar066@gmail.com>
2026-04-04 18:27:20 +0000

    chore: remove commented-out VisitorTracker code

--- components/VisitorTracker.jsx
+++ components/VisitorTracker.jsx
@@ -1,32 +1,3 @@
-// 'use client';
-
-// import { useEffect } from 'react';
-
-// export default function VisitorTracker({ profileId }) {
-//   useEffect(() => {
-//     // Check if user has already been counted in this session
-//     const hasVisited = sessionStorage.getItem('visitor_counted');
-    
-//     if (!hasVisited && profileId) {
-//       // Call API to increment count
-//       fetch('/api/increment-visitor', {
-//         method: 'POST',
-//         headers: { 'Content-Type': 'application/json' },
-//         body: JSON.stringify({ profileId }),
-//       })
-//       .then(() => {
-//         // Mark as counted for this session
-//         sessionStorage.setItem('visitor_counted', 'true');
-//       })
-//       .catch((error) => {
-//         console.error('Error tracking visitor:', error);
-//       });
-//     }
-//   }, [profileId]);
-
-//   return null;
-// }
-
 'use client';
 
 import { useEffect } from 'react';
